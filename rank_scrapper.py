cd62a1d528ce067eac4ccf16803ff2a13024d5b6
8UCK37 <104382050+8UCK37@users.noreply.github.com>
2023-06-09 15:55:50 +0530

    getting all the relevant data

--- rank_scrapper.py
+++ rank_scrapper.py
@@ -1,21 +1,56 @@
 import requests
 from bs4 import BeautifulSoup
 
+player_data={}
+def extract_values_from_div(div):
+    values = []
+    inner_divs = div.find_all('div')
 
-url = 'https://tracker.gg/valorant/profile/riot/CaRNaGePero%2369420/overview'
+    if inner_divs:
+        for inner_div in inner_divs:
+            value = inner_div.get('set')
+            if value:
+                values.append(value)
+    
+    if values:
+        player_data['topAgent']=values[0]
+        player_data['hours']=values[1]
+        player_data['winPer']=values[2]
+        player_data['kd']=values[3]
+        player_data['adr']=values[4]
+        player_data['acs']=values[5]
+    return values
+
+
+url = 'https://tracker.gg/valorant/profile/riot/Ez4aCE%237365/overview'
 response = requests.get(url)
 html = response.text
 
 
 soup = BeautifulSoup(html, 'html.parser')
 
-div_element = soup.find('div', class_='rating-entry__rank-info')
+rankDiv = soup.find('div', class_='rating-entry__rank-info')
+rank = rankDiv.find('div', class_='value').text.strip()
 
-value_text = div_element.find('div', class_='value').text.strip()
-
-print(value_text)  
-
-
- 
+# print('rank: ',rank)  
+player_data['rank']=rank
 
 
+agentDiv = soup.find('div', class_='st-content__category')
+matchesPlayed = agentDiv.find('div', class_='label')
+timesPlayed=matchesPlayed.text.strip()
+player_data['macthesPlayed']=timesPlayed
+
+
+divs = soup.find_all('div', class_='st-content__item')
+
+for div in divs:
+    imageDiv=div.find('div',class_='image')
+    img_tag = imageDiv.find('img')
+    if img_tag:
+        src_url = img_tag['src']
+        # print(src_url)
+        player_data['agentUrl']=src_url
+    values = extract_values_from_div(div)
+    break
+print(player_data)  
