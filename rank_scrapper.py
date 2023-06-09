d0b6e7cd311d55d3bdb39b8253129fbc6a62f064
8UCK37 <104382050+8UCK37@users.noreply.github.com>
2023-06-09 20:06:09 +0530

    parsed giant stats data

--- rank_scrapper.py
+++ rank_scrapper.py
@@ -14,12 +14,33 @@ def extract_values_from_div(div):
     
     if values:
         player_data['topAgent']=values[0]
-        player_data['hours']=values[1]
-        player_data['winPer']=values[2]
-        player_data['kd']=values[3]
-        player_data['adr']=values[4]
-        player_data['acs']=values[5]
+        player_data['agentHours']=values[1]
+        player_data['winPerWithTopG']=values[2]
+        player_data['TopGKd']=values[3]
+        player_data['TopGadr']=values[4]
+        player_data['TopGacs']=values[5]
     return values
+
+def extract_values_from_giant_stats(div):
+    values = {}
+    name_spans = div.find_all('span', class_='name')
+    value_spans = div.find_all('span', class_='value')
+
+    for name_span, value_span in zip(name_spans, value_spans):
+        name = name_span.text.strip()
+        value = value_span.text.strip()
+        values[name] = value
+    
+    return values
+
+def extract_giant_stats(giant_stats):
+    
+    stat_divs = giant_stats.find_all('div', class_='stat align-left giant expandable')
+    for stat_div in stat_divs:
+        values = extract_values_from_giant_stats(stat_div)
+        player_data.update(values)
+
+
 
 
 url = 'https://tracker.gg/valorant/profile/riot/Ez4aCE%237365/overview'
@@ -53,4 +74,7 @@ for div in divs:
         player_data['agentUrl']=src_url
     values = extract_values_from_div(div)
     break
+
+giant_statsDiv = soup.find('div', class_='giant-stats')
+extract_giant_stats(giant_statsDiv)
 print(player_data)  
